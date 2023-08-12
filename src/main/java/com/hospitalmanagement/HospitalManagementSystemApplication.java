35ab2e882d8f55e44b0515ca75cac7f3cd437f0c
Ayush Yadav <119103994+Ayushyadav197@users.noreply.github.com>
2023-08-12 13:47:39 +0530

    Ticket 01 : Project Based Structure
    Ticket 01 : Project Based Structure

---
+++ src/main/java/com/hospitalmanagement/HospitalManagementSystemApplication.java
@@ -0,0 +1,20 @@
+package com.hospitalmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class HospitalManagementSystemApplication {
+	
+	public static void main(String[] args)  {
+		SpringApplication.run(HospitalManagementSystemApplication.class, args);
+		System.out.println("hi");
+	}
+
+}
+
+// You have to take the Backup of this project.
+// This backup should be in running mode in another workspace.
+// make clone to it--> and perform your task here in another workspace.
+//(As all you wish you can do the chnages here)
